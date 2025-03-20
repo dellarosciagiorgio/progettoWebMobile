2727b3324ff8fb0b716d33700286878533173050
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2025-03-20 14:46:57 +0100

    Create componenti per la get di Eventi e Sagre

---
+++ gestoreSagre/src/components/getEventi/Eventi.svelte
@@ -0,0 +1,20 @@
+<script>
+    import jsonData from '../../../../responses/GetEventi.json';
+</script>
+
+<div>
+    {#if jsonData && jsonData.data && Array.isArray(jsonData.data)}
+        {#each jsonData.data as evento}
+        	<div>
+          	    <h3>ID evento: {evento.idEvento}</h3>
+          	    <p>ID sagra: {evento.idSagra}</p>
+          	    <p>Data evento: {evento.dataEvento.split('T')[0]}</p>
+          	    <p>Informazioni aggiuntive: {evento.informazioniAggiuntive}</p>
+          	    <p>ID stocks biglietto: {evento.idStocksBiglietto === null ? 'N/A' : evento.idStocksBiglietto}</p>
+          	    <hr/>
+            </div>
+        {/each}
+    {:else}
+        <p>Nessun evento presente.</p>
+    {/if}
+</div>
---
+++ gestoreSagre/src/components/getSagre/Sagre.svelte
@@ -0,0 +1,21 @@
+<script>
+	import jsonData from '../../../../responses/GetSagre.json';
+</script>
+
+<div>
+    {#if jsonData && jsonData.data && Array.isArray(jsonData.data)}
+        {#each jsonData.data as sagra}
+      		<div>
+        		<h3>ID: {sagra.idSagra}</h3>
+        		<p>Nome: {sagra.nomeSagra}</p>
+        		<p>Descrizione: {sagra.descrizione}</p>
+        		<p>ID feedbacks: {sagra.idFeedbacks === null ? 'N/A' : sagra.idFeedbacks}</p>
+        		<p>ID sagrai: {sagra.idEventi === null ? 'N/A' : sagra.idEventi}</p>
+        		<p>ID organizzatore: {sagra.idOrganizzatore === null ? 'N/A' : sagra.idOrganizzatore}</p>
+        		<hr/>
+      		</div>
+    	{/each}
+    {:else}
+        <p>Nessuna sagra presente.</p>
+    {/if}
+</div>
