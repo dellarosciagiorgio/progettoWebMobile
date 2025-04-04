68535a60cc4b02a28afab5e96ddf8ee2063339e7
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2025-04-04 16:22:26 +0200

    c

--- gestoreSagre/src/components/getEventi/getEventiFuturi/EventiFuturi.svelte
+++ gestoreSagre/src/components/getEventi/getEventiFuturi/EventiFuturi.svelte
@@ -1,47 +1,6 @@
 <script>
-    import { onMount } from "svelte";
-    import { getData } from "../../../lib/get.js";
-
-    let eventi = [];
-    let loading = true;
-    let error = "";
-
-    onMount(async () => {
-        const data = await getData("eventi");
-        if (data) {
-            eventi = data.data;
-			console.log(eventi);
-        } else {
-            error = "Errore nel recupero dei dati.";
-        }
-        loading = false;
-    });
+    import Eventi from "../Eventi.svelte";
+    const eventiFuturi = true;
 </script>
 
-{#if loading}
-	 <div class="d-flex justify-content-center my-5">
-		<div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;">
-		  <span class="visually-hidden">Caricamento...</span>
-		</div>
-	  </div>
-{:else if error}
-    <p style="color: red;">{error}</p>
-{:else}
-    <div class="container mt-4">
-        <div class="row">
-            {#each eventi as evento}
-                {#if new Date(evento.dataEvento) >= new Date()}
-                    <div class="col-md-4 mb-4">
-                        <div class="card" style="width: 100%;">
-                            <img src="/imgs/evento.jpg" class="card-img-top" alt="Immagine dell'evento">
-                            <div class="card-body">
-                                <h5 class="card-title">{evento.dataEvento.split('T')[0]}</h5>
-                                <p class="card-text">{evento.informazioniAggiuntive}</p>
-                            </div>
-                        </div>
-                    </div>
-                {/if}
-            {/each}
-        </div>
-    </div>
-{/if}
+<Eventi prossimiEventi={eventiFuturi}/>
