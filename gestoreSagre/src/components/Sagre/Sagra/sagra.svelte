2c0a9c5e16abbb2fdba6cd00d774a11778dc5a07
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2025-04-15 18:21:11 +0200

    GET valutazione sagra

--- gestoreSagre/src/components/Sagre/Sagra/sagra.svelte
+++ gestoreSagre/src/components/Sagre/Sagra/sagra.svelte
@@ -1,14 +1,18 @@
 <script>
     import Eventi from "../../Eventi/eventi.svelte";
     import { onMount } from 'svelte';
+    import { getData } from "../../../lib/get.js";
 
-    export let id;
-
-    onMount(() => {
-        id = new URLSearchParams(window.location.search).get('id');
+    let id;
+    let error = null;
+    let valutazione = null;
+    
+    onMount(async () => {
+        let data = await getData(`sagra/ratingbyid/${new URLSearchParams(window.location.search).get('id')}`);
+        data ? valutazione = data.data : error = "Errore nel recupero dei dati.";
     });
 </script>
 
-id sagra: {id}
+Valutazione: {valutazione}
 
 <Eventi idSagra={new URLSearchParams(window.location.search).get('id')} percorso={''}/>
