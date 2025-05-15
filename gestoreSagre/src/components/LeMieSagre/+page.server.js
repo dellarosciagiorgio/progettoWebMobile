4407fead5054b953df775348e39d5a07e6c7846e
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2025-05-15 15:34:35 +0200

    Rifinitura codice components/

--- gestoreSagre/src/components/LeMieSagre/+page.server.js
+++ gestoreSagre/src/components/LeMieSagre/+page.server.js
@@ -1,17 +1,10 @@
-// +page.server.js per LeMieSagre
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ cookies }) {
-  // Controlla se l'utente è autenticato
-  const authToken = cookies.get('authToken');
-  const userRole = cookies.get('userRole');
-  
-  // Se l'utente non è un organizzatore autenticato, reindirizza alla home
-  if (!authToken || userRole !== 'Organizzatore') {
-    throw redirect(302, '/Home');
-  }
-  
-  // Non carichiamo i dati lato server, lo faremo nel componente client
-  return {};
+	const authToken = cookies.get('authToken');
+	const userRole = cookies.get('userRole');
+	if (!authToken || userRole !== 'Organizzatore')
+		throw redirect(302, '/Home');
+	return {};
 } 
